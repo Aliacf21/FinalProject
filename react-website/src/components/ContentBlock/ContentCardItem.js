20377a2b7595e9589956a1b6215ca46ad52993c4
Aliacf21 <51883210+Aliacf21@users.noreply.github.com>
2020-12-11 12:27:51 -0500

    better readability

--- react-website/src/components/ContentBlock/ContentCardItem.js
+++ react-website/src/components/ContentBlock/ContentCardItem.js
@@ -25,7 +25,7 @@ else {
       <li className='cards__item2'>
         <div className='cards__item__link2'>
            <div className='cards__item__info2'>
-            <h6 className='cards__item__text2'>{props.Description} </h6>
+            <p className='cards__item__text2'>{props.Description} </p>
           </div>
 
              
