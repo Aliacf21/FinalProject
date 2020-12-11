7dfbdf067b9a2f0c20d8d4f866df87a7efd52ede
Aliacf21 <51883210+Aliacf21@users.noreply.github.com>
2020-12-11 12:25:46 -0500

    content

--- react-website/src/components/InfoSection/data.js
+++ react-website/src/components/InfoSection/data.js
@@ -54,8 +54,8 @@ export const BunBunBakeShop_overview = {
 	role: "Designer, Programmer",
 	team: "",
 	tools: "Figma, Javascript, HTML/CSS, react-bootstrap", 
-	description: "I am a senior at and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
-
+	description: "The project centered around understanding stakeholder needs in order to build a website for a small bakery.",
+	alt: "Pumpkin pie"
 };
 
 export const BunBunBakeShop_problems = {
@@ -79,7 +79,11 @@ export const BunBunBakeShop_solutions = {
 export const IAT_overview = {
 	id: "Overview",
 	link3: "IAT",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	team: "Erik Thiessen (PI) | Aisha Nazir (Research Assistant)",
+	role: "Research Assistant",
+	tools: "PEBL", 
+	alt: "Pittsburgh skyline",
+	description: "We measured implicit gender attitudes using the Implicit Association Task (IAT). The IAT will was administered using PEBL (the Psychology Experiment Building Language), which is a software tool for presenting experimental stimuli and recording participants’ responses to them"
 
 };
 
@@ -91,13 +95,14 @@ export const IAT_Research_Question = {
 
 export const IAT_Experiment = {
 	id: "Experiment",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "The IAT is a tool used in social psychology in order to assess people's implicit associations between concepts (Greenwald, Mcghee, & Schwartz, 1998). The IAT is performed by having participants rapidly categorize two target concepts with an attribute (e.g., the concept 'male' or 'female' with the attribute 'logical'), such that easier pairings (i.e., faster responses) are interpreted as more strongly associated in memory than more difficult pairings (i.e., slower responses) (Greenwald, Mcghee, & Schwartz, 1998).The participants performed 6 blocks of tasks (see table to the left for a description of each block) each composed of 80 trials. Participants were instructed to select either “1” or “9” depending on the target mappings. Blocks 1, 2, 4, and 5 acted as baseline measures of the participants' reaction times. Blocks 3 and 6 measured implicit attitudes by having participants categorize faces and attributes in a way that was either congruent or incongruent with gender stereotypes. The congruent block had participants categorize male faces and positive attributes together vs. female faces and negative attributes, while the incongruent block had participants categorize male faces and negative attribute together vs. the female faces and positive attributes. There were two versions of the task, which counterbalanced the order in which participants perform the congruent and incongruent block."
+
 
 };
 
 export const IAT_Results = {
 	id: "Results",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "Our results found an ingroup bias in both genders, however women showed more ingroup preference than men."
 
 };
 
@@ -105,9 +110,10 @@ export const programming_overview = {
 	id: "Overview",
 	link3: "15112 Term Project",
 	team: "",
-	role: "Designer, Programmer",
-	tools: "Figma, Javascript, HTML/CSS, react-bootstrap", 
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	role: "Programmer",
+	tools: "Python", 
+	alt: "a picture of a farm",
+	description: "I used KNN for my term project to classify digits drawn on the computer  in order to foster counting skills"
 
 };
 
@@ -132,82 +138,98 @@ export const programming_solutions = {
 export const ResearchProject_overview = {
 	id: "Overview",
 	link3: "coffee",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	alt: "coffe cup",
+	tools: "Figma",
+	role: "Researcher",
+	team: "Qinye Yu | Crystal Lin",
+	description: "Our research sought to explore the effects of coffee cues on people’s cognitive performance."
 
 };
 
 export const ResearchProject_Research_Question = {
 	id: "Reserch Question",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "Many people rely on coffee in their daily lives as a stimulant. According to a study commissioned by the National Coffee Association, 64% of American adults report they drink coffee every day with an average consumption of 3.1 cups per day. While direct intake of caffeine is known to cause physiological arousal, studies show that environmental cues related to coffee also affect people’s thoughts and behaviors (Chan & Maglio, 2019). Our research sought to explore the effects of coffee cues on people’s cognitive performance. Understanding this problem allows us to learn more about enhancing performance without using caffeine or other central neural system stimuli. Our study also examined how reliance and impression of coffee can influence our performance."
 
 };
 
 export const ResearchProject_Experiment = {
 	id: "Experiment",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "In this study, we used a 2 (Visual cue: coffee visual cue and no cue) * 2 (Survey order: pre-task and post-task) between-subject factorial design and measure participants’ reaction time and accuracy on a repeated digit vigilance task. According to Nehlig’s study on caffeine’s effect on human cognition (Nehlig, A. 2004), among a variety of cognitive tasks, caffeine has the most significant and consistent effect on vigilance tasks. This helped us determine the effects of coffee as a visual cue on cognitive performance and how people’s expectations might serve as a mediator."
 
 };
 
 export const ResearchProject_Results = {
 	id: "Results",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "We randomly assigned 80 participants into four conditions. The coffee and pre-task survey condition (C1) had 19 participants. The coffee and post-task survey condition (C2) had 20 participants. The water and pre-task survey condition (W1) had 21 participants. The water and post-task survey condition (W2) had 20 participants. The participants completed 5 blocks in the task, with 100 trials in each block (10 positives and 90 negatives). The data collection thus produced 50 positive trials and 450 negative trials in total for each participant. No participant or trial was excluded given that all hit trials were within three standard deviations from the mean response time. We calculated the hit rate, false alarm rate, average response time for hits, d’ and beta for each participant and then compiled the data. For 7 participants with a hit rate of 1 or false alarm rate of 0, we are unable to calculate d’ and beta. We did not exclude them because most of these participants were in the coffee condition, and excluding them would have made our analysis significantly biased. Therefore for these participants, we substituted hit rate with 0.98, which was 1 - 1/(total positive trials) and false alarm rate with 0.0022, which is 1/(total negative trials). These adjusted d’ and beta values accounted for less than 10% of our total samples. "
 
 };
 
 export const Shuttle_overview = {
 	id: "Overview",
 	link3: "shuttle",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	alt: "Picture of moorewood",
+	tools: "Figma",
+	role: "User Researcher",
+	team: "Dean Dijour | John Han | Bruce Liu | Kathryn Phelps",
+	description: "We conducted contextual interviews with 5 CMU students, and launched a survey with nearly 30 responses, to better gauge how students viewed the transportation situation to the Pittsburgh International Airport. "
 
 };
 
 export const Shuttle_problems = {
 	id: "Problems",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "Better understand how CMU student's experience airport transportation, and what hurdles they face."
 
 };
 
 export const Shuttle_process = {
 	id: "Process",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "We used the walk the wall technique, affinity diagramming, the 5 second test, and experience prototyping techniques. After interviewing over a dozen CMU students, we are confident that we have identified a serious problem, requiring a solution markedly better than the status quo. Our research led us to generate numerous insights about the cost, time-sensitivity, and stress levels of airport transportation from CMU, leading us to ideate a more intuitive solution to the problems our participants described."
 
 };
 
 export const Shuttle_solutions = {
 	id: "Solution",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "A dedicated airport shuttle and companion application, exclusively for CMU students, which is fast, reliable, and spacious allows students to have a quick and comfortable ride."
 
 };
 
 export const StemIAT_overview = {
 	id: "Overview",
 	link3: "StemIAT",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	team: "Erik Thiessen (PI) | Aisha Nazir (Research Assistant)",
+	role: "Research Assistant",
+	tools: "PEBL", 
+	alt: "Doha Skyline",
+	description: "Research project which examines gender bias in the middle east vs. United states concerning liberal arts and stem"
 
 };
 
 export const StemIAT_Research_Question = {
 	id: "Reserch Question",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "Do men and women in United States, Qatar and Saudi Arabia differ in their implicit associations between gender and STEM?"
 
 };
 
 export const StemIAT_Experiment = {
 	id: "Experiment",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "We used the IAT task and changed the valences from positive/negative to liberal arts/stem"
 
 };
 
 export const StemIAT_Results = {
 	id: "Results",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	description: "Our preliminary results so far have shown that in the united states both men and women associate stem with men and liberal arts with women"
 
 };
 
 export const Test_overview = {
 	id: "Overview",
 	link3: "Test",
-	description: "I am a senior at Carnegie Mellon University in cognitive science and human-computer interaction with a minor in computer science. I am passionate about research, software, and user-centered design."
+	team: "",
+	role: "Programmer",
+	tools: "PEBL", 
+	alt: "Mosque",
+	description: "Refactored code to support Right-to-Left writing system with the IAT"
 
 };
 
